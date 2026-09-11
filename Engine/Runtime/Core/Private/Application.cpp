8fab1fa7279bb0cc20f0280c329b4a402a7db823
Alejandro Suárez Perales <asu4net@gmail.com>
2026-09-11 19:39:46 +0200

    Test window setup for SDL

--- Engine/Runtime/Core/Private/Application.cpp
+++ Engine/Runtime/Core/Private/Application.cpp
@@ -1,8 +1,46 @@
 #include "Runtime/Application.h"
+#include "SDL3/SDL.h"
 
 void Application::Init()
 {
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+  SDL_Window* window = SDL_CreateWindow(
+      "SDL3 + OpenGL",
+      1280,
+      720,
+      SDL_WINDOW_OPENGL
+      );
+
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+
+  bool running = true;
+
   OnInit();
+
+  while (running)
+  {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_EVENT_QUIT)
+        running = false;
+    }
+
+    //glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+    //glClear(GL_COLOR_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(window);
+  }
+
+  SDL_GL_DestroyContext(context);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 };
 
 void Application::Exit()
